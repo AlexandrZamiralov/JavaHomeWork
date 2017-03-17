70c15fc9891e56b673a43c828609b95eeeab1e10
AlexandrZamiralov <ff.akila@mail.ru>
2017-03-18 00:58:28 +0400

    Create Edge.java

---
+++ someGraph/Diametr/Edge.java
@@ -0,0 +1,12 @@
+public class Edge {
+    int from;
+    int to;
+    int weight;
+
+
+    Edge(int from, int to){
+        this.from = from;
+        this.to = to;
+        this.weight = 1;
+    }
+}
